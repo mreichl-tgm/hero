13b5e23fc5913a53b33f89490e4a645d7c3ee05c
Markus Re1 <re1@a1.net>
2016-10-16 13:46:13 +0200

    Add Attack and Projectile Controllers
    Add PlayerAttack and ProjectileController classes to unity and provide a simple bullet to shoot

---
+++ Unity/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+	public GameObject shot;
+	public float range;
+	public float fireRate;
+
+	private Transform shotSpawn;
+	private float nextFire;
+
+	void Start()
+	{
+		shotSpawn = GetComponent<Transform> ();
+	}
+
+	void Update()
+	{
+		if (Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Destroy(Instantiate(shot, shotSpawn.position, shotSpawn.rotation), range);
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		Rigidbody2D rb2D = GetComponent<Rigidbody2D> ();
+		rb2D.velocity = new Vector2(0, 1) * speed;
+	}
+}
