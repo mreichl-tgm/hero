227809b6c94e0abaa7d57e78e799c82600206461
Markus Re1 <re1@a1.net>
2016-10-16 12:48:04 +0200

    Use Axis instead of fixed vectors
    Going to use getAxis instead of Vector positions for smooth movement and code reduction

---
+++ Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	private Vector2 moveDirection;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() 
+	{
+		moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		rb2d.velocity = moveDirection * speed;
+	}
+}
