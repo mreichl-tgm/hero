dee2e9bfb1d4638859d14ee74e11eed39aa23060
Markus Re1 <re1@a1.net>
2016-12-30 00:13:04 +0100

    Add Inventory and Equipment

---
+++ Unity/Hero/Assets/Scripts/Equipment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Equipment : MonoBehaviour {
+	[SerializeField]
+	private GameObject weapon;
+	[SerializeField]
+	private GameObject shield;
+	[SerializeField]
+	private GameObject ability;
+	[SerializeField]
+	private GameObject headArmor;
+	[SerializeField]
+	private GameObject handArmor;
+	[SerializeField]
+	private GameObject chestArmor;
+	[SerializeField]
+	private GameObject legArmor;
+	[SerializeField]
+	private GameObject footArmor;
+}
---
+++ Unity/Hero/Assets/Scripts/Inventory.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+	[SerializeField]
+	private GameObject[] inventory;
+}
