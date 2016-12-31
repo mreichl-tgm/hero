93ce7ab48a8c9c3f6a796c2c5ba7dc92f5fbe675
Markus Re1 <re1@a1.net>
2016-12-31 01:31:06 +0100

    Add Cursor
    Add a cursor gameobject and its controller for easier access to the mousePosition

---
+++ Unity/Hero/Assets/Scripts/CursorController.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour {
+	void Update() {
+		transform.position = Input.mousePosition;
+	}
+}
--- Unity/Hero/Assets/Scripts/Effect/ActivatableEffect/Attack.cs
+++ Unity/Hero/Assets/Scripts/Effect/ActivatableEffect/Attack.cs
@@ -5,20 +5,29 @@ public class Attack : ActivatableEffect {
 	[SerializeField]
 	private GameObject projectile;
 	[SerializeField]
+	private GameObject source;
+	[SerializeField]
+	private GameObject target;
+	[SerializeField]
 	private float speed;
 
+	private Vector3 destination;
+	private Vector3 origin;
+
+	void Awake() {
+		destination = target.transform.position;
+		origin = source.transform.position;
+	}
+
 	public override void Activate() {
-		Vector2 mousePos = Camera.main.WorldToScreenPoint(transform.position);
-		Vector3 mousePosNormalized = new Vector3(Input.mousePosition.x - mousePos.x, Input.mousePosition.y - mousePos.y, 0).normalized;
+		Vector3 force = Camera.main.WorldToScreenPoint(origin - destination).normalized;
 
-		Vector3 origin = transform.position - mousePosNormalized;
-
-		float angle = Mathf.Atan2(mousePosNormalized.y, mousePosNormalized.x) * Mathf.Rad2Deg;
+		float angle = Mathf.Atan2(force.y, force.x) * Mathf.Rad2Deg;
 
 		projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
 
 		Rigidbody2D projectile2D;
 		projectile2D = Instantiate (projectile.GetComponent<Rigidbody2D>(), origin, projectile.transform.rotation) as Rigidbody2D;
-		projectile2D.AddForce(mousePosNormalized * speed);
+		projectile2D.AddForce(force * speed);
 	}
 }
